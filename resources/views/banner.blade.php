f80d207da1e3b72e423ae9c95b78ccb7de428ae3
Shipu Ahamed <shipu@10minuteschool.com>
2023-08-13 04:02:36 +0600

    update banner

--- resources/views/banner.blade.php
+++ resources/views/banner.blade.php
@@ -1,16 +1,11 @@
-<div class="max-w-full h-auto py-2 mt-3 duration-300 ease-out bg-white shadow-sm sm:py-0 sm:h-10 rounded-lg">
+<div class="max-w-full h-auto py-2 mt-3 duration-300 ease-out bg-white dark:bg-gray-900 shadow-sm sm:py-0 sm:h-10 rounded-lg">
     <div class="flex items-center justify-between w-full h-full px-3 mx-auto max-w-7xl ">
         <a href="https://github.com/Shipu/dinero" target="_blank" class="flex flex-col w-full h-full text-xs leading-6 text-black duration-150 ease-out sm:flex-row sm:items-center opacity-80 hover:opacity-100">
             <span class="flex items-center">
                 <svg class="w-5 h-5 mr-1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g fill="none" stroke="none"><path d="M10.1893 8.12241C9.48048 8.50807 9.66948 9.5633 10.4691 9.68456L13.5119 10.0862C13.7557 10.1231 13.7595 10.6536 13.7968 10.8949L14.2545 13.5486C14.377 14.3395 15.4432 14.5267 15.8333 13.8259L17.1207 11.3647C17.309 11.0046 17.702 10.7956 18.1018 10.8845C18.8753 11.1023 19.6663 11.3643 20.3456 11.4084C21.0894 11.4567 21.529 10.5994 21.0501 10.0342C20.6005 9.50359 20.0352 8.75764 19.4669 8.06623C19.2213 7.76746 19.1292 7.3633 19.2863 7.00567L20.1779 4.92643C20.4794 4.23099 19.7551 3.52167 19.0523 3.82031L17.1037 4.83372C16.7404 4.99461 16.3154 4.92545 16.0217 4.65969C15.3919 4.08975 14.6059 3.39451 14.0737 2.95304C13.5028 2.47955 12.6367 2.91341 12.6845 3.64886C12.7276 4.31093 13.0055 5.20996 13.1773 5.98734C13.2677 6.3964 13.041 6.79542 12.658 6.97364L10.1893 8.12241Z" stroke="currentColor" stroke-width="1.5"></path><path d="M12.1575 9.90759L3.19359 18.8714C2.63313 19.3991 2.61799 20.2851 3.16011 20.8317C3.70733 21.3834 4.60355 21.3694 5.13325 20.8008L13.9787 11.9552" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M5 6.25V3.75M3.75 5H6.25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18 20.25V17.75M16.75 19H19.25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></g></svg>
-                &nbsp;Hello, thank you for visiting this demo project. This is an open source project. You can find the source code on&nbsp;GitHub.
-                <span class="hidden w-px h-4 mx-3 rounded-full sm:block bg-neutral-200"></span>
+                &nbsp;Hello, thank you for visiting this demo project and data will be reset every 15 minute minutes. This is an open source project. You can find the source code on&nbsp;GitHub.
+                <x-filament::icon class="w-5 h-5" icon="lucide-mouse-pointer-click"/>
             </span>
         </a>
-        <iframe
-            src="https://ghbtns.com/github-btn.html?user=shipu&repo=dinero&type=star&count=true"
-            class="flex justify-center w-16 h-6 p-1.5"
-            title="GitHub">
-        </iframe>
     </div>
 </div>
