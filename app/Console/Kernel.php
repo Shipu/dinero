f0d1535c76665d8cdfa11f048e2de93a84d82146
Shipu Ahamed <shipu@10minuteschool.com>
2023-08-13 04:02:51 +0600

    reduce schedule time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         if(config('app.demo')) {
-            $schedule->command('migrate:fresh --seed')->everySixHours();
+            $schedule->command('migrate:fresh --seed')->everyFifteenMinutes();
         }
     }
 
